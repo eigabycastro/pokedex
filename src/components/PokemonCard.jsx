65ab42bb4e4208a89b755340a89f5cd64a61fbee
unknown <Administrador@UNI-LABF-7463.fuvs.br>
2026-05-07 21:12:35 -0300

    feat: estrutura inicial da pokedex

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+  return (
+    <div>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+        alt="Pikachu"
+      />
+      <h2>#025 — Pikachu</h2>
+      <p>Tipo: Elétrico</p>
+    </div>
+  )
+}
+
+export default PokemonCard
